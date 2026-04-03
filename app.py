c0cc4c52732cb5a09664dbcfb354aa2fa4a791ef
pr4389152-ux <pr4389152@gmail.com>
2026-04-03 18:25:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,15 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'pooja_secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['UPLOAD_FOLDER'] = 'static'
 
 db = SQLAlchemy(app)
-with app.app_context():
+
+# -------- DATABASE CREATE FIX --------
+@app.before_first_request
+def create_tables():
     db.create_all()
+
 # -------- MODELS --------
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -47,10 +52,11 @@ class Result(db.Model):
     username = db.Column(db.String(100))
     score = db.Column(db.Integer)
 
-# -------- ADMIN LOGIN --------
+# -------- ADMIN --------
 ADMIN_USER = "pooja"
 ADMIN_PASS = "admin123"
 
+# -------- HOME --------
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -59,39 +65,55 @@ def home():
 @app.route('/register', methods=['GET','POST'])
 def register():
     if request.method == 'POST':
-        user = User(
-            name=request.form['name'],
-            trade=request.form['trade'],
-            password=request.form['password']
-        )
-        db.session.add(user)
-        db.session.commit()
-        return redirect('/login')
+        try:
+            user = User(
+                name=request.form.get('name'),
+                trade=request.form.get('trade'),
+                password=request.form.get('password')
+            )
+            db.session.add(user)
+            db.session.commit()
+            return redirect('/login')
+        except Exception as e:
+            return str(e)
     return render_template('register.html')
 
 # -------- LOGIN --------
 @app.route('/login', methods=['GET','POST'])
 def login():
     if request.method == 'POST':
-        user = User.query.filter_by(name=request.form['name'], password=request.form['password']).first()
+        user = User.query.filter_by(
+            name=request.form.get('name'),
+            password=request.form.get('password')
+        ).first()
+
         if user:
             session['user'] = user.name
             session['trade'] = user.trade
             return redirect('/dashboard')
+
     return render_template('login.html')
 
 # -------- DASHBOARD --------
 @app.route('/dashboard')
 def dashboard():
+    if 'user' not in session:
+        return redirect('/login')
+
     videos = Video.query.filter_by(trade=session['trade']).all()
     pdfs = PDF.query.filter_by(trade=session['trade']).all()
+
     return render_template('dashboard.html', videos=videos, pdfs=pdfs, name=session['user'])
 
 # -------- PROFILE --------
 @app.route('/profile')
 def profile():
+    if 'user' not in session:
+        return redirect('/login')
+
     user = User.query.filter_by(name=session['user']).first()
     results = Result.query.filter_by(username=session['user']).all()
+
     return render_template('profile.html', user=user, results=results)
 
 # -------- ADMIN LOGIN --------
@@ -113,40 +135,53 @@ def admin():
         file = request.files['file']
         file_type = request.form['type']
 
-        if file_type == 'video':
-            file.save(os.path.join('static/videos', file.filename))
-            db.session.add(Video(title=request.form['title'], filename=file.filename, trade=request.form['trade']))
+        if file:
+            if file_type == 'video':
+                path = os.path.join('static/videos', file.filename)
+                file.save(path)
+                db.session.add(Video(title=request.form['title'], filename=file.filename, trade=request.form['trade']))
 
-        elif file_type == 'pdf':
-            file.save(os.path.join('static/pdfs', file.filename))
-            db.session.add(PDF(title=request.form['title'], filename=file.filename, trade=request.form['trade']))
+            elif file_type == 'pdf':
+                path = os.path.join('static/pdfs', file.filename)
+                file.save(path)
+                db.session.add(PDF(title=request.form['title'], filename=file.filename, trade=request.form['trade']))
 
-        db.session.commit()
+            db.session.commit()
 
     videos = Video.query.all()
     pdfs = PDF.query.all()
+
     return render_template('admin.html', videos=videos, pdfs=pdfs)
 
 # -------- DELETE --------
 @app.route('/delete_video/<int:id>')
 def delete_video(id):
-    db.session.delete(Video.query.get(id))
-    db.session.commit()
+    video = Video.query.get(id)
+    if video:
+        db.session.delete(video)
+        db.session.commit()
     return redirect('/admin')
 
 @app.route('/delete_pdf/<int:id>')
 def delete_pdf(id):
-    db.session.delete(PDF.query.get(id))
-    db.session.commit()
+    pdf = PDF.query.get(id)
+    if pdf:
+        db.session.delete(pdf)
+        db.session.commit()
     return redirect('/admin')
 
 # -------- MCQ --------
 @app.route('/mcq', methods=['GET','POST'])
 def mcq():
+    if 'user' not in session:
+        return redirect('/login')
+
     questions = MCQ.query.all()
+
     if request.method == 'POST':
         score = 0
         wrong = []
+
         for q in questions:
             ans = request.form.get(str(q.id))
             if ans == q.answer:
@@ -158,6 +193,7 @@ def mcq():
         db.session.commit()
 
         return render_template('result.html', score=score, wrong=wrong)
+
     return render_template('mcq.html', questions=questions)
 
 # -------- ADD MCQ --------
@@ -173,6 +209,7 @@ def add_mcq():
             answer=request.form['answer']
         ))
         db.session.commit()
+
     return render_template('add_mcq.html')
 
 # -------- PYQ --------
