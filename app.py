4403545c79173fa8f997a40ec3b3adc4a553533b
pr4389152-ux <pr4389152@gmail.com>
2026-04-03 20:22:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,14 @@ def dashboard():
     pdfs = PDF.query.filter_by(trade=session['trade']).all()
 
     return render_template('dashboard.html', videos=videos, pdfs=pdfs, name=session['user'])
-
+# admin login 
+@app.route('/admin_login', methods=['GET','POST'])
+def admin_login():
+    if request.method == 'POST':
+        if request.form['username'] == "pooja" and request.form['password'] == "admin123":
+            session['admin'] = True
+            return redirect('/admin')
+    return render_template('admin_login.html')
 # -------- LOGOUT --------
 @app.route('/logout')
 def logout():
